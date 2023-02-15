79ed3a154cfae3e5cacaa84917c4760f97afd999
Bravura Lion <70280114+mrpowershell@users.noreply.github.com>
2023-02-15 11:26:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ client = discord.Client(intents=intents)
 
 server_sets = {
     'de': ['de1', 'de2', 'de3', 'de4', 'de5'],
-    'en': ['en1', 'en2', 'en3', 'en4', 'en5', 'en6', 'en7', 'en8']
+    'en': ['en1', 'en2', 'en3', 'en4', 'en5', 'en6', 'en7', 'en8'],
     'pl': ['pl1', 'pl2', 'pl3', 'pl4', 'pl5', 'pl8']
 }
 
