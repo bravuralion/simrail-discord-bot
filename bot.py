f9d1316a7d00f73dacc7bb7660f3acf43991a8ae
Bravura Lion <70280114+mrpowershell@users.noreply.github.com>
2023-02-15 11:50:29 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,20 +9,24 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+# Define server sets to check
 server_sets = {
     'de': ['de1', 'de2', 'de3', 'de4', 'de5'],
     'en': ['en1', 'en2', 'en3', 'en4', 'en5', 'en6', 'en7', 'en8'],
     'pl': ['pl1', 'pl2', 'pl3', 'pl4', 'pl5', 'pl8']
 }
 
+# Time interval between checks (in seconds)
 update_interval = 30
 
+# Function to get data from server API
 async def get_data(server):
     async with aiohttp.ClientSession() as session:
         async with session.get(f'https://panel.simrail.eu:8084/stations-open?serverCode={server}') as resp:
             text = await resp.text()
             return json.loads(text)
 
+# Function to check which stations are open and send a message to the specified channel
 async def check_stations(server_list, servers, last_updated, channel):
     messages = defaultdict(list)
 
@@ -31,10 +35,11 @@ async def check_stations(server_list, servers, last_updated, channel):
         for station in data['data']:
             if not station['DispatchedBy']:
                 messages[station['Name']].append(servers[server])
-
+    # If there are no messages, return the last update time
     if not messages:
         return last_updated
-
+    
+    # Format the messages
     output = ""
     for station, servers in sorted(messages.items()):
         if len(servers) == 1:
@@ -46,6 +51,7 @@ async def check_stations(server_list, servers, last_updated, channel):
     last_updated = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     message = f"Stations currently open (last updated at {last_updated}):\n" + output
     message += "This bot was developed and provided by http://simrail.community"
+    # Send the message to the specified channel
     try:
         async for msg in channel.history(limit=1):
             if msg.author == client.user:
@@ -63,11 +69,13 @@ async def on_ready():
 @client.event
 async def on_message(message):
     global channel_id
+    # If the message starts with !set_channel, set the channel to send messages to
     if message.content.startswith('!set_channel'):
         channel = message.channel_mentions[0]
         channel_id = channel.id
         await message.channel.send(f"Channel set to {channel.mention}")
         await message.channel.purge(limit=3)
+    # If the message starts with !stations, set the servers to check
     elif message.content.startswith('!stations'):
         if not channel_id:
             await message.channel.send("Channel not set. Use !set_channel <channel_mention> first.")
